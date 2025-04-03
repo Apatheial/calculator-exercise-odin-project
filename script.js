438187b84c8d2f1c18c7518a8720dc1bbf676e96
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-04-02 23:24:04 -0300

    feat: create functions for arithmetic operations
    Implemented addition, subtraction, multiplication,
    and division functions.

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = ( firstNum, secondNum ) => {
+    return firstNum + secondNum;
+};
+
+const subtract = ( firstNum, secondNum ) => {
+    return firstNum - secondNum;
+};
+
+const multiply = ( firstNum, secondNum ) => {
+    return firstNum * secondNum;
+};
+
+const divide = ( firstNum, secondNum ) => {
+    return firstNum / secondNum;
+};
